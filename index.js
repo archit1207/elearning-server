c4d464253646d387775f0e1321670de38362e885
archit1207 <architmishra1207@gmail.com>
2025-11-25 22:45:39 +0530

    frontend-backend gatway

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ import { instance } from "./config/razorpay.js";
 const app = express();
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
-app.use(express.json());
 
 app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+    origin: ["https://masteredge.vercel.app", "*"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.use(express.json());
 
 const port = process.env.PORT;
 
