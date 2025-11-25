72c874c66898a41aac8b49712efa167de50711a1
archit1207 <architmishra1207@gmail.com>
2025-11-25 22:30:57 +0530

    Cors error 2

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use(express.json());
 
 app.use(cors({
-    origin: "*",
-    credentials: true
-}
-));
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 const port = process.env.PORT;
 
